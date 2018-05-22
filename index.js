173f10ef191a228e66f13f705b82b66ec17b790c
Victor Popkov <victor@popkov.me>
2018-05-22 13:59:08 +0300

    Add webpack v4 support

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 
 const juice = require('juice');
 
-function HtmlWebpackInlinerPlugin (options) {
+function HtmlWebpackInlinerPlugin(options) {
     // Initialize
 }
 
 HtmlWebpackInlinerPlugin.prototype.apply = compiler => {
-	compiler.plugin('compilation', compilation => {
-		compilation.plugin('html-webpack-plugin-after-html-processing', (htmlPluginData, callback) => {
-			htmlPluginData.html = juice(htmlPluginData.html);
-			callback(null, htmlPluginData);
-		});
-	});
+    (compiler.hooks
+        ? compiler.hooks.compilation.tap.bind(compiler.hooks.compilation, 'html-webpack-inline-style-plugin')
+        : compiler.plugin.bind(compiler, 'compilation'))(compilation => {
+
+        (compilation.hooks
+            ? compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync.bind(compilation.hooks.htmlWebpackPluginAfterHtmlProcessing, 'html-webpack-inline-style-plugin')
+            : compilation.plugin.bind(compilation, 'html-webpack-plugin-after-html-processing'))((htmlPluginData, callback) => {
+            htmlPluginData.html = juice(htmlPluginData.html);
+            callback(null, htmlPluginData);
+        });
+    });
 };
 
 module.exports = HtmlWebpackInlinerPlugin;
